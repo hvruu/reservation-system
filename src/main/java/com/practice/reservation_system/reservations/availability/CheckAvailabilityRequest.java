e89a33915d7339a55d037aa48e2e4f3e5ceebffc
hvruu <foxyucus87@gmail.com>
2025-12-06 22:03:15 +0500

    changed Long to RoomEntity

--- src/main/java/com/practice/reservation_system/reservations/availability/CheckAvailabilityRequest.java
+++ src/main/java/com/practice/reservation_system/reservations/availability/CheckAvailabilityRequest.java
@@ -1,9 +1,12 @@
 package com.practice.reservation_system.reservations.availability;
 
+import com.practice.reservation_system.reservations.ReservationEntity;
+import com.practice.reservation_system.rooms.RoomEntity;
+
 import java.time.LocalDate;
 
 public record CheckAvailabilityRequest(
-        Long roomId,
+        RoomEntity room,
         LocalDate startDate,
         LocalDate endDate
 ) {
