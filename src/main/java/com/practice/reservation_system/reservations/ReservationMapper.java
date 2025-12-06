eb1e473e45940817942f846087eae99e2c179428
hvruu <foxyucus87@gmail.com>
2025-12-06 22:08:17 +0500

    new logic to map layers

--- src/main/java/com/practice/reservation_system/reservations/ReservationMapper.java
+++ src/main/java/com/practice/reservation_system/reservations/ReservationMapper.java
@@ -1,17 +1,34 @@
 package com.practice.reservation_system.reservations;
 
+import com.practice.reservation_system.rooms.RoomMapper;
+import com.practice.reservation_system.rooms.RoomRepository;
+import com.practice.reservation_system.users.UserMapper;
+import com.practice.reservation_system.users.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ReservationMapper {
+
+    private final UserRepository userRepository;
+    private final RoomRepository roomRepository;
+    private final UserMapper userMapper;
+    private final RoomMapper roomMapper;
+
+    public ReservationMapper(UserRepository userRepository, RoomRepository roomRepository, UserMapper userMapper, RoomMapper roomMapper) {
+        this.userRepository = userRepository;
+        this.roomRepository = roomRepository;
+        this.userMapper = userMapper;
+        this.roomMapper = roomMapper;
+    }
 
     public Reservation toDomain(
             ReservationEntity reservationEntity
     ){
         return new Reservation(
                 reservationEntity.getId(),
-                reservationEntity.getUserId(),
-                reservationEntity.getRoomId(),
+                userMapper.toDomain(reservationEntity.getUser()),
+                roomMapper.toDomain(reservationEntity.getRoom()),
                 reservationEntity.getStartDate(),
                 reservationEntity.getEndDate(),
                 reservationEntity.getStatus()
@@ -19,15 +36,20 @@ public class ReservationMapper {
     }
 
     public ReservationEntity toEntity(
-            Reservation reservation
+            ReservationSave reservation
     ){
-        return new ReservationEntity(
+        var reservationToMap = new ReservationEntity(
                 reservation.id(),
-                reservation.userId(),
-                reservation.roomId(),
                 reservation.startDate(),
                 reservation.endDate(),
                 reservation.status()
         );
+
+        reservationToMap.setUser(userRepository.findById(reservation.userId())
+                .orElseThrow(() -> new EntityNotFoundException("Not found user with id=" + reservation.userId())));
+        reservationToMap.setRoom(roomRepository.findById(reservation.roomId())
+                .orElseThrow(() -> new EntityNotFoundException("Not found room with id=" + reservation.roomId())));
+
+        return reservationToMap;
     }
 }
